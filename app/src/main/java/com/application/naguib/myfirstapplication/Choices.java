ccebe1be6b3b87c171e4571c1fcaa7024873334a
Anas Chakfeh <anas.chakfeh@gmail.com>
2018-11-11 21:19:23 +0100

    add choices data

--- app/src/main/java/com/application/naguib/myfirstapplication/Choices.java
+++ app/src/main/java/com/application/naguib/myfirstapplication/Choices.java
@@ -1,7 +1,14 @@
 package com.application.naguib.myfirstapplication;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.Set;
 
 public class Choices extends AppCompatActivity {
 
@@ -9,5 +16,10 @@ public class Choices extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choices);
+        SharedPreferences sharedpreferences = getSharedPreferences("pass_the_hat", Context.MODE_PRIVATE);
+        Set<String> set = sharedpreferences.getStringSet("teams", null);
+        String item = set.iterator().next();
+        TextView ll = (TextView)findViewById(R.id.textView4);
+        ll.setText(item);
     }
 }
--- app/src/main/java/com/application/naguib/myfirstapplication/TeamNames.java
+++ app/src/main/java/com/application/naguib/myfirstapplication/TeamNames.java
@@ -1,15 +1,20 @@
 package com.application.naguib.myfirstapplication;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.ArraySet;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
 import android.widget.LinearLayout;
+
+import java.util.Set;
 
 public class TeamNames extends AppCompatActivity {
 
@@ -39,6 +44,20 @@ public class TeamNames extends AppCompatActivity {
     }
 
     public void onButtonClicked(View view) {
+        SharedPreferences sharedpreferences = getSharedPreferences("pass_the_hat", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedpreferences.edit();
+        Set<String> set = new ArraySet<String>();
+        LinearLayout ll = (LinearLayout) findViewById(R.id.team_name_layout);
+
+        for(int i = 0; i < ll.getChildCount(); i++) {
+            EditText et = (EditText)ll.getChildAt(i);
+            set.add(et.getText().toString());
+        }
+
+
+        editor.putStringSet("teams", set);
+        editor.commit();
+
         Intent intent = new Intent(this, Choices.class);
         startActivity(intent);
 
